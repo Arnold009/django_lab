938a020f2207decbb036387f1cd0e7c0cc6774d7
Arnold009 <arnoldmacwan009@gmail.com>
2024-07-02 20:06:00 +0530

    db and model setup with user model

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,88 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 
-# Create your models here.
 
 class Demo(models.Model):
     id = models.AutoField(primary_key=True)
     name =models.CharField(max_length=15)
+    
+    
+
+################  creating a custom user model instead of using default provided by django ################
+
+class CustomUserManager(UserManager):
+    ## method is a private method intended for internal use within the user manager class. It directly creates and saves a user instance to the database.
+    def _create_user(self,email,password,**extra_fields):
+        if not email:
+            raise ValueError("Email couldn't fetched !!")
+        email =  self.normalize_email(email)
+        user = self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    ## method is a public method that provides an interface for creating a regular user. It sets default values and then calls the _create_user method to perform the actual creation and saving of the user instance.
+    def create_user(self, email=None,password=None,**extra_fields):
+        extra_fields.setdefault('is_superuser', False )
+        extra_fields.setdefault('is_staff', False )
+        return self._create_user(email=email,password=password,**extra_fields)
+
+    def create_superuser(self,email=None,password=None,**extra_fields):
+        extra_fields.setdefault('is_superuser', True )
+        extra_fields.setdefault('is_staff', True )
+        return self._create_user(email=email,password=password,**extra_fields)
+
+
+class User(AbstractBaseUser,PermissionsMixin):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(max_length=30,unique=True)
+    password = models.CharField(max_length=255)
+    firstname = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
+    img = models.CharField(max_length=255 , null=True)
+    phone = models.CharField(
+        max_length=10,
+        validators=[MinLengthValidator(10), MaxLengthValidator(10)],
+        unique=True
+    )
+    address = models.ForeignKey('FullAddress' , on_delete=models.CASCADE,blank=True , null=True , related_name="user_fullAddress")
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True )
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
+
+    class Meta:
+        verbose_name='User'
+        verbose_name_plural='Users'
+
+
+
+
+class FullAddress(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE ,related_name="FullAddress")
+    state = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    country = models.CharField(max_length=30 , null=True)
+    area = models.CharField(max_length=40)
+    landmark = models.CharField(max_length=40 , null=True)
+    residency = models.CharField(max_length=30)
+    house_no = models.CharField(max_length=10)
+    pincode =models.CharField(max_length=8)
+    
+    def __str__(self):
+        return f"{self.house_no}, {self.residency}, {self.area}, {self.city}, {self.state}"
+    
+    class Meta:
+        verbose_name = 'FullAddress'
+        verbose_name_plural = 'FullAddresses'
--- myproject/settings.py
+++ myproject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,12 +55,12 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'myproject.urls'
 
 ##define your customised model
-# AUTH_USER_MODEL = 'myapp.User'
+AUTH_USER_MODEL = 'myapp.User'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['AppEcomm/templates'],
+        'DIRS': ['myapp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,9 +123,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
